687b116ba4556e54cffa268bd785cf307e9d1557
Sseankzs <sseankong52@gmail.com>
2025-08-02 21:38:33 +0800

    app.py update

--- app.py
+++ app.py
@@ -2,11 +2,9 @@
 
 from flask import Flask, request, jsonify
 from firebase_utils import init_firebase
-from vector_matcher import match_vector
 import config
 from user_routes import user_bp
 from merchant_routes import merchant_bp
-from palm_routes import palm_bp
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -15,7 +13,6 @@ db = init_firebase(config.FIREBASE_CRED_PATH)
 
 app.register_blueprint(user_bp)
 app.register_blueprint(merchant_bp)
-app.register_blueprint(palm_bp)
 
 if __name__ == "__main__":
     app.run(host=config.HOST, port=config.PORT, debug=config.DEBUG)
